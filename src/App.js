5d5ba551024057e1b8dd7d36d09f2019aa2d984f
Talha Shahid <talhashahidch@gmail.com>
2022-07-29 22:19:29 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import ToDoLists from './ToDoLists';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  //"inputList" contains the value which user has entered
+  const [inputList, setInputList] = useState("");
+  //"items" is an array which consists of all the stuff user has entered
+  const [items, setItems] = useState([]);
+
+  //When user type something, "itemEvent" will be triggered thereby setting "inputList"
+  const itemEvent = (event)=>{
+    setInputList(event.target.value)
+  };
+  //When user clicks on button, "listOfItems" will be triggered and will append "inputList" array by
+  // adding 
+  const listOfItems = ()=>{
+    setItems((oldItems)=>{
+      return [...oldItems, inputList]
+    });
+    setInputList("");
+  }
+
+  // When user clicks on "cross" icon
+  const deleteItems = (id)=> {
+
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem, index)=>{
+        return index !== id;
+      });
+    });
+    
+  }
+
+  //rest of the code
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder='Add an Item' value={inputList} onChange={itemEvent}/>
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+            {items.map((itemval, index)=>{
+              return <ToDoLists key={index} id={index} text={itemval} onSelect= {deleteItems}/>
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
